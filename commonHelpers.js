d37306c2010fd92a815028370c4b2642eca83705
nataliia-smalchenko <137941820+nataliia-smalchenko@users.noreply.github.com>
2023-12-10 00:36:54 +0000

    Deploying to gh-pages from @ nataliia-smalchenko/goit-advancedjs-hw-04@6f01660565f9e679c8f6106b8ea772cfd07edfad 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,28 @@
-import"./assets/vendor-f070e68e.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import{S as y,a as m,i as b}from"./assets/vendor-f67ecabd.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&l(n)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();const u="37184762-fd326293791c817732540ec51",s={form:document.querySelector(".js-search-form"),gallery:document.querySelector(".js-gallery"),loadBtn:document.querySelector(".load-more")};let d;s.form.addEventListener("submit",a=>{a.preventDefault();const e=a.currentTarget;d=e.elements.searchQuery.value,w(d),e.reset()});s.loadBtn.addEventListener("click",()=>{L(d)});const g=new y(".gallery > .photo-card > a");let i,f;async function w(a){s.loadBtn.hidden=!0,s.gallery.innerHTML="",i=1;try{const e=await m.get("https://pixabay.com/api/",{params:{key:u,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0,page:i,per_page:40}});f=Math.ceil(e.data.totalHits/40),c("info",`Hooray! We found ${e.data.totalHits} images.`);const o=e.data.hits;o.length?(s.gallery.innerHTML=p(o),g.refresh(),s.loadBtn.hidden=!1):c("error","Sorry, there are no images matching your search query. Please try again.")}catch{c("error","Oops! Something went wrong! Please try again.")}}async function L(a){s.loadBtn.hidden=!0,i+=1;try{const o=(await m.get("https://pixabay.com/api/",{params:{key:u,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0,page:i,per_page:40}})).data.hits;console.log(f),s.gallery.insertAdjacentHTML("beforeend",p(o)),g.refresh(),i<f&&(s.loadBtn.hidden=!1)}catch(e){console.log(e),c("error","Oops! Something went wrong! Please try again.")}}function p(a){return a.map(({webformatURL:e,largeImageURL:o,tags:l,likes:t,views:r,comments:n,downloads:h})=>`<div class="photo-card">
+        <a class="gallery-link" href="${o}">
+          <img class="gallery-image" src="${e}" alt="${l}" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              <br />
+              ${t}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              <br />
+              ${r}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              <br />
+              ${n}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              <br />
+              ${h}
+            </p>
+          </div>
+        </a>
+      </div>`).join("")}function c(a,e){let o;a==="error"?o="#f58e82":o="#9dfab5",b.show({messageColor:"#262121",backgroundColor:o,messageSize:"18px",position:"bottomRight",progressBar:!1,animateInside:!1,transitionIn:"fadeIn",transitionOut:"fadeOut",timeout:3e3,targetFirst:!1,message:e})}
 //# sourceMappingURL=commonHelpers.js.map
