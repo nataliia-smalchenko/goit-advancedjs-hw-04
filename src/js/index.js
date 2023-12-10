6f01660565f9e679c8f6106b8ea772cfd07edfad
Nataliia Smalchenko <137941820+lana-daalang@users.noreply.github.com>
2023-12-10 02:36:17 +0200

    task resolved

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,163 @@
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const key = '37184762-fd326293791c817732540ec51';
+
+const elements = {
+  form: document.querySelector('.js-search-form'),
+  gallery: document.querySelector('.js-gallery'),
+  loadBtn: document.querySelector('.load-more'),
+};
+
+let search;
+
+elements.form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+  search = form.elements.searchQuery.value;
+  searchImages(search);
+  form.reset();
+});
+
+elements.loadBtn.addEventListener('click', () => {
+  searchMoreImages(search);
+});
+
+const gallery = new SimpleLightbox('.gallery > .photo-card > a');
+
+let page;
+let maxPages;
+
+async function searchImages(text) {
+  elements.loadBtn.hidden = true;
+  elements.gallery.innerHTML = '';
+  page = 1;
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key,
+        q: text,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: 40,
+      },
+    });
+    maxPages = Math.ceil(response.data.totalHits / 40);
+    showMessage('info', `Hooray! We found ${response.data.totalHits} images.`);
+    const data = response.data.hits;
+    if (!data.length) {
+      showMessage(
+        'error',
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      elements.gallery.innerHTML = createPhotoMarkup(data);
+      gallery.refresh();
+      elements.loadBtn.hidden = false;
+    }
+  } catch (error) {
+    showMessage('error', 'Oops! Something went wrong! Please try again.');
+  }
+}
+
+async function searchMoreImages(text) {
+  elements.loadBtn.hidden = true;
+  page += 1;
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key,
+        q: text,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: 40,
+      },
+    });
+    const data = response.data.hits;
+    console.log(maxPages);
+    elements.gallery.insertAdjacentHTML('beforeend', createPhotoMarkup(data));
+    gallery.refresh();
+
+    if (page < maxPages) {
+      elements.loadBtn.hidden = false;
+    }
+  } catch (error) {
+    console.log(error);
+    showMessage('error', 'Oops! Something went wrong! Please try again.');
+  }
+}
+
+function createPhotoMarkup(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              <br />
+              ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              <br />
+              ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              <br />
+              ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              <br />
+              ${downloads}
+            </p>
+          </div>
+        </a>
+      </div>`;
+      }
+    )
+    .join('');
+}
+
+function showMessage(type, message) {
+  let color;
+  if (type === 'error') {
+    color = '#f58e82';
+  } else {
+    color = '#9dfab5';
+  }
+
+  iziToast.show({
+    messageColor: '#262121',
+    backgroundColor: color,
+    messageSize: '18px',
+    position: 'bottomRight',
+    progressBar: false,
+    animateInside: false,
+    transitionIn: 'fadeIn',
+    transitionOut: 'fadeOut',
+    timeout: 3000,
+    targetFirst: false,
+    message: message,
+  });
+}
